76b44dec59155c87e72cbc1f2237cfa2a31bc47d
woxmat <61566551+woxmat@users.noreply.github.com>
2020-02-29 20:30:32 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -100,7 +100,7 @@ rsync -a ../clone/* ./wp-content/${PROJECT_TYPE}s/${REPO_NAME}
 echo "Add remote"
 
 # git remote add ${repo} git@git.kinsta.com:${repo}/${REPO_INSTALL}.git
-git remote add ${repo} ssh://${SSH_NAME}:${SSH_IP}:${SSH_PORT}/www/${STAGE_ROOT}/public/${REPO_NAME}.git
+git remote add ${repo} ssh://${SSH_NAME}@${SSH_IP}:${SSH_PORT}/www/${STAGE_ROOT}/public/${REPO_NAME}.git
 
 git config --global user.email CI_COMMITTER_EMAIL
 git config --global user.name CI_COMMITTER_NAME
