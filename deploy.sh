c022c3b7a36df648f7ebcabb03e567192d391803
woxmat <61566551+woxmat@users.noreply.github.com>
2020-05-22 23:44:53 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -84,7 +84,7 @@ if [ ! -d "./wp-content/themes" ]; then
     mkdir ./wp-content/themes
 fi
 
-composer update --working-dir=./wp-content/${PROJECT_TYPE}s/${REPO_NAME}
+composer update --working-dir=../clone
 rsync -a ../clone/* ./wp-content/${PROJECT_TYPE}s/${REPO_NAME}
 
 # Stage, commit, and push to wpengine repo
