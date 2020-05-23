b90cc65eaa1c6ca1580b2a05cc395608722eb012
woxmat <61566551+woxmat@users.noreply.github.com>
2020-05-23 09:19:28 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,6 @@ fi
 # Begin from the ~/clone directory
 # this directory is the default your git project is checked out into by Codeship.
 cd ~/clone
-wget --output-document=composer.json https://raw.githubusercontent.com/officialwoxmat/kinsta-codeship-continuous-deployment/master/composer-template.json
 
 # Get official list of files/folders that are not meant to be on production if $EXCLUDE_LIST is not set.
 if [[ -z "${EXCLUDE_LIST}" ]];
@@ -65,9 +64,10 @@ if [ "$?" != "0" ] ; then
     kill -SIGINT $$
 fi
 
-# Move the gitignore and files to the deployments folder
+# Move the gitignore and composer.json files to the deployments folder
 cd ~/deployment
 wget --output-document=.gitignore https://raw.githubusercontent.com/officialwoxmat/kinsta-codeship-continuous-deployment/master/gitignore-template.txt
+wget --output-document=composer.json https://raw.githubusercontent.com/officialwoxmat/kinsta-codeship-continuous-deployment/master/composer-template.json
 
 # Delete plugin/theme if it exists, and move cleaned version into deployment folder
 rm -rf /wp-content/${PROJECT_TYPE}s/${REPO_NAME}
