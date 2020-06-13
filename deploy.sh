0f3b2d39ac59f2a8ae2285558e44eb143a0b8112
woxmat <61566551+woxmat@users.noreply.github.com>
2020-06-13 07:15:24 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ else
 fi
 
 # Check we're in the ~/clone directory, and switch to it if not.
-# ~/clone is the default your git project is checked out into by Codeship.
+# This is the default your git project is checked out into by Codeship.
 cd ~/clone
 
 # Get official list of files/folders that are not meant to be on production if $EXCLUDE_LIST is not set.
